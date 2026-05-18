134086729d786af46b564938c4b4945b0bc09c36
Justin Mammarella <jmamma@gmail.com>
2026-05-19 09:03:54 +1000

    Reduce GFX line helper overhead

--- Adafruit_GFX.cpp
+++ Adafruit_GFX.cpp
@@ -156,7 +156,7 @@ void Adafruit_GFX::writeFastHLine(int16_t x, int16_t y, int16_t w,
   // Overwrite in subclasses if startWrite is defined!
   // Example: writeLine(x, y, x+w-1, y, color);
   // or writeFillRect(x, y, w, 1, color);
-  drawFastHLine(x, y, w, color);
+  writeLine(x, y, x + w - 1, y, color);
 }
 
 void Adafruit_GFX::writeFillRect(int16_t x, int16_t y, int16_t w, int16_t h,
@@ -186,7 +186,7 @@ void Adafruit_GFX::drawFastVLine(uint8_t x, uint8_t y, uint8_t h,
 // should sort endpoints or call drawLine() instead
 void Adafruit_GFX::drawFastHLine(int16_t x, int16_t y, int16_t w,
                                  uint16_t color) {
-  // Update in subclasses if desired!
+  // MCL's SSD1305 path has no transaction hooks; avoid no-op virtual calls.
   writeLine(x, y, x + w - 1, y, color);
 }
 
@@ -205,18 +205,9 @@ void Adafruit_GFX::fillScreen(uint16_t color) {
 
 void Adafruit_GFX::drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
                             uint16_t color) {
-  // Update in subclasses if desired!
-  if (x0 == x1) {
-    if (y0 > y1)
-      _swap_int16_t(y0, y1);
-    drawFastVLine(x0, y0, y1 - y0 + 1, color);
-  } else if (y0 == y1) {
-    if (x0 > x1)
-      _swap_int16_t(x0, x1);
-    drawFastHLine(x0, y0, x1 - x0 + 1, color);
-  } else {
-    writeLine(x0, y0, x1, y1, color);
-  }
+  startWrite();
+  writeLine(x0, y0, x1, y1, color);
+  endWrite();
 }
 
 // Draw a circle outline
